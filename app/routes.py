f2b12f0c0e9f531aadc0a862c0258a2ed773e7f9
Kat <kmunson13@gmail.com>
2023-04-21 11:42:54 -0400

    Add planet class and setup flask routes

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, moon_number):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moon_number = moon_number
+        
+
+planets = [
+    Planet(1, "Mercury", "Dark gray", 0),
+    Planet(2, "Venus", "Light yellowish", 0),
+    Planet(3, "Earth", "Blue and green", 1)
+    ]
+
+planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planet_bp.route("", methods=["GET"])
+def handle_planets():
+    planet_response = []
+    for planet in planets:
+        planet_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moon_number": planet.moon_number
+        })
+    return jsonify(planet_response)
