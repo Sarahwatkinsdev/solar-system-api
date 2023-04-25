79da4b18692da440748438f2ffbe0175f7686c43
Kat <kmunson13@gmail.com>
2023-04-25 14:06:32 -0400

    Add route to read one planet by id

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, moon_number):
@@ -14,6 +14,18 @@ planets = [
     Planet(3, "Earth", "Blue and green", 1)
     ]
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planet_bp.route("", methods=["GET"])
@@ -27,3 +39,14 @@ def handle_planets():
             "moon_number": planet.moon_number
         })
     return jsonify(planet_response)
+
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def handle_planets_by_id(planet_id):
+    planet = validate_planet(planet_id)
+    
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "moon_number": planet.moon_number
+    }
