47669bd0dfb7274358964523a5fa5b0f8d27ad2a
Sarah Watkins <sarahwwatkins@gmail.com>
2023-04-28 12:11:24 -0400

    New route with a post message - to get all planets

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,21 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
+
+planets_bp = Blueprint("planets", __name__, url_prefix="planets")
+
+@planets_bp.route("", methods=["POST"])
+def handle_planets():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        moon_number=request_body["moon_number"])
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
 
 # class Planet:
 #     def __init__(self, id, name, description, moon_number):
